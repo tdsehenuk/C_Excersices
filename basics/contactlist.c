19b64086e7a4bd47d430ce398bd54edbe965a007
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-29 22:31:44 -0400

    skeleton for a contactlist

---
+++ basics/contactlist.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+typedef struct {
+    int phone_number[10];
+    char names[25];
+    char emails[25];
+} Contact;
+
+
+
+int main () {
+
+
+    printf("welcome to a contact list app\n");
+    printf("here we store peoples nnumbers nmaes and emails ");
+
+
+
+}
