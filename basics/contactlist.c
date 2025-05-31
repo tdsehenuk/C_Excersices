733ec7003cd3eab0ab54268a970ddcbc82114e12
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-05-30 23:40:55 -0400

    asd

--- basics/contactlist.c
+++ basics/contactlist.c
@@ -19,14 +19,14 @@ int main () {
     printf("Welcome to a contact list app!\n");
     printf("Here we store peoples numbers names and emails!\n");
     printf("Here is what the app can do. Please select a choice:\n");
-    printf("1.)Add a Contact\n2.)Show all Contacts\n3.)Edit a Contact\n4.)Delete Contact\n5.)Search for Contact\n6.)Quit!\n\n");
 
     while(1) {
 
-    printf("Welcome back to the menu. Please select a choice:\n");
-    printf("1.)Add a Contact\n2.)Show all Contacts\n3.)Edit a Contact\n4.)Delete Contact\n5.)Search for Contact\n6.)Quit!\n\n");
+    printf("\nWelcome to the menu. Please select a choice:\n");
+    printf("1.)Add a Contact\n2.)Show all Contacts\n3.)Edit a Contact\n4.)Delete Contact\n5.)Search for Contact\n6.)Quit!\n");
     int choice = 0;
     scanf("%d", &choice);
+   
         switch (choice) {
             case 1:
                 printf("You selected to add a contact!\n");
@@ -44,7 +44,8 @@ int main () {
                 break;
 
             case 4:
-                printf("You selected choice 4!\n");
+                printf("You selected to delete a contact!\n");
+                delete_contact();
                 break;
 
              case 5:
@@ -53,7 +54,7 @@ int main () {
 
             case 6:
                 printf("You selected choice 6!\n");
-                break;
+                return 0;
 
             default:
                 printf("You did not select one a choices!");
@@ -89,14 +90,37 @@ void add_contact() {
     buffer[strcspn(buffer, "\n")] = '\0';
     strcpy(contact_list[contact_counter].name, buffer);
 
+    printf("Contact Added Succesfully\n");
     contact_counter++;
 }
 
 void show_contacts() {
     for(int i = 0; i < contact_counter; i++) {
+        printf("Index: %d\n", i);
         printf("Name: %s\n", contact_list[i].name);
         printf("Email: %s\n", contact_list[i].email);
         printf("Number: %s\n", contact_list[i].phone_number);
         printf("--------------\n");
     }
 }
+
+void delete_contact() {
+    int index_delete;
+    while (1) { //loop until valid number is gotten 
+        printf("please enter what index you would like to delete");
+        scanf("%d", &index_delete);
+        if(contact_counter == 0) {
+            printf("The list is empty! Nothing to delete here.");
+        } else if(index_delete >= 1 && index_delete <= contact_counter) break;
+
+        
+    }
+
+    for( ; index_delete < contact_counter - 1; index_delete++) {
+            contact_list[index_delete] = contact_list[index_delete+1];
+        }
+    printf("Contact deleted successfully.\n");
+    contact_counter--;
+}
+    
+
